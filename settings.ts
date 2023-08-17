544f3f0b9329b34aa0d137558e31385d338a0de3
Marco Schlicht <git@privacymail.dev>
2023-08-17 22:46:25 +0200

    added features to ignore and replace urls
    fixes #15

--- settings.ts
+++ settings.ts
@@ -2,6 +2,11 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 import ObsidianLinkArchivePlugin from "./main";
 import { defaultArchiveText } from "./constants";
 
+export interface LinkReplaceRule {
+    originalPattern: string;
+    replacementPattern: string;
+}
+
 export const enum ArchiveOptions {
 	Wayback,
 	Archiveis,
@@ -12,11 +17,15 @@ export const enum ArchiveOptions {
 export interface LinkArchivePluginSettings {
 	archiveOption: ArchiveOptions;
 	archiveText: string;
+	linkReplaceRules: LinkReplaceRule[];
+	ignoreUrlPatterns: string[];
 }
 
 export const defaultSettings: LinkArchivePluginSettings = {
 	archiveOption: ArchiveOptions.Archiveis,
-	archiveText: defaultArchiveText
+	archiveText: defaultArchiveText,
+	linkReplaceRules: [],
+	ignoreUrlPatterns: []
 }
 
 
@@ -30,54 +39,118 @@ export class LinkArchiveSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const plugin: ObsidianLinkArchivePlugin = (this as any).plugin;
-
-        const { containerEl } = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		// add archive link text customization option
+		const archiveSettingsSection = containerEl.createDiv();
+		archiveSettingsSection.createEl("h2", { text: "Archive Settings" });
 
-		containerEl.createEl("h2", {text: "Archive Settings"});
+		new Setting(archiveSettingsSection)
+		.setName("Link text")
+		.setDesc("The text of the archive links")
+		.addText(text =>
+				 text
+		.setValue(plugin.settings.archiveText)
+		.onChange(async value => {
+			console.log(`Link text: ${value}`);
+			plugin.settings.archiveText = value;
+			await plugin.saveSettings();
+		}));
 
-        new Setting(containerEl)
-            .setName("Link text")
-            .setDesc("The text of the archive links")
-            .addText(text =>
-                text
-                    .setValue(plugin.settings.archiveText)
-                    .onChange(async value => {
-                        console.log(`Link text: ${value}`);
-                        plugin.settings.archiveText = value;
-                        await plugin.saveSettings();
-                    }));
+		// link ignore rules
+		const linkIgnoreSection = containerEl.createDiv();
+		linkIgnoreSection.createEl("h2", { text: "Link Ignore Rules" });
 
-		// new Setting(containerEl)
-		// 	.setName('Archive Provider')
-		// 	.setDesc('Choose a provider for the link archive')
-		// 	.addDropdown((dropdown) => {
-		// 		const options: Record<ArchiveOptions, string> = {
-		// 			0: "Internet Archive",
-		// 			1: "archive.is",
-		// 			2: "Both"
-		// 		};
+		new Setting(linkIgnoreSection)
+		.setName("Ignore URL Patterns")
+		.setDesc("Enter regular expressions to match URLs that should be ignored by the plugin")
+		.addButton(button => {
+			button.setButtonText("Add Pattern").onClick(() => {
+				plugin.settings.ignoreUrlPatterns.push("");
+				this.display();
+			});
+		});
 
-		// 		dropdown
-		// 			.addOptions(options)
-		// 			.setValue(plugin.settings.archiveOption.toString())
-		// 			.onChange(async (value) => {
-		// 				console.log('Archive option: ' + value);
-		// 				plugin.settings.archiveOption = +value;
-		// 				await plugin.saveSettings();
-		// 				this.display();
-		// 		})
-		// 	});
+		// add ignore url patterns
+		for (let i = 0; i < plugin.settings.ignoreUrlPatterns.length; i++) {
+			const pattern = plugin.settings.ignoreUrlPatterns[i];
 
-		containerEl.createEl("h2", {text: "About Link Archive"});
+			const setting = new Setting(containerEl)
+			.setName(`Pattern ${i + 1}`)
+			.addText(text => text
+					 .setPlaceholder("URL Pattern")
+					 .setValue(pattern)
+					 .onChange(async value => {
+						 plugin.settings.ignoreUrlPatterns[i] = value;
+						 await plugin.saveSettings();
+					 }));
 
-		containerEl.createEl("p", {text: "This plugin archives links in your note so they're available to you even if the original site goes down or gets removed."});
+					 setting.addButton(button => {
+						 button.setIcon("cross")
+						 .onClick(async () => {
+							 plugin.settings.ignoreUrlPatterns.splice(i, 1);
+							 await plugin.saveSettings();
+							 this.display();
+						 });
+					 });
+		}
 
-		containerEl.createEl("a", {text: "Open GitHub repository", href: "https://github.com/tomzorz/obsidian-link-archive"});
+		// add link replacement rules
+		const linkReplacementSection = containerEl.createDiv();
+		linkReplacementSection.createEl("h2", { text: "Link Replacement Rules" });
 
-		// TODO github support and ko-fi
+		new Setting(linkReplacementSection)
+		.setDesc("Configure rules to replace specific links before archiving.")
+		.addButton(button => {
+			button.setButtonText("Add Rule").onClick(() => {
+				plugin.settings.linkReplaceRules.push({
+					originalPattern: "",
+					replacementPattern: ""
+				});
+				this.display();
+			});
+		});
+
+		// add link replacement rules
+		for (let i = 0; i < plugin.settings.linkReplaceRules.length; i++) {
+			const rule = plugin.settings.linkReplaceRules[i];
+
+			const setting = new Setting(containerEl)
+			.setName(`Rule ${i + 1}`)
+			.addText(text => text
+					 .setPlaceholder("Original Pattern")
+					 .setValue(rule.originalPattern)
+					 .onChange(async value => {
+						 rule.originalPattern = value;
+						 await plugin.saveSettings();
+					 }))
+					 .addText(text => text
+							  .setPlaceholder("Replacement Pattern")
+							  .setValue(rule.replacementPattern)
+							  .onChange(async value => {
+								  rule.replacementPattern = value;
+								  await plugin.saveSettings();
+							  }));
+
+							  setting.addButton(button => {
+								  button.setIcon("cross")
+								  .onClick(async () => {
+									  plugin.settings.linkReplaceRules.splice(i, 1);
+									  await plugin.saveSettings();
+									  this.display();
+								  });
+							  });
+		}
+
+		// add about section
+		const aboutSection = containerEl.createDiv();
+		aboutSection.createEl("h2", { text: "About Link Archive" });
+
+		aboutSection.createEl("p", {text: "This plugin archives links in your note so they're available to you even if the original site goes down or gets removed."});
+
+		aboutSection.createEl("a", {text: "Open GitHub repository", href: "https://github.com/tomzorz/obsidian-link-archive"});
+
 	}
 }
